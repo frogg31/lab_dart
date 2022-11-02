99b49768abbcfcb0f7c15db6b8aeb32979449a2b
frogg31 <113858357+frogg31@users.noreply.github.com>
2022-11-02 19:53:34 +0300

    feat(lw0 fizzbuzz):create fizzbuzz

--- lw0/fizzbuzz.dart
+++ lw0/fizzbuzz.dart
@@ -1,22 +1,18 @@
-import "dart:io";
-
-void fizzbuzz(int number) {
-  if (number % 3 == 0 && number % 5 == 0) {
-    print("fizzbuzz");
-  } else if (number % 3 == 0) {
-    print("fizz");
-  } else if (number % 5 == 0) {
-    print("buz");
-  } else {
-    print(number);
+void fizzbuzz(int n) {
+  for(int i=1;i<=n;i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("fizzbuzz");
+    } else if (i % 3 == 0) {
+      print("fizz");
+    } else if (i % 5 == 0) {
+      print("buz");
+    } else {
+      print(i);
+    }
   }
 }
 
+
 void main() {
-  print("введите количество цифр: ");
-  int n = int.parse(stdin.readLineSync()!);
-  print("\n");
-  for (int i = 1; i <= n; i++) {
-    fizzbuzz(i);
-  }
+  return fizzbuzz(15);
 }
